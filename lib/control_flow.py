bd14430d5231d130397050c6ccdba9adb28095ca
William Gitau <dontysh@gmail.com>
2024-09-26 01:32:30 +0300

    Passes all tests.

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -9,9 +9,9 @@ def admin_login(username, password):
         return "Access denied"
 
 def hows_the_weather(temperature):
-    if temperature<40:
+    if temperature < 40:
         return "It's brisk out there!"
-    elif temperature >=40 and temperature<=65:
+    elif temperature >= 40 and temperature <= 65:
         return "It's a little chilly out there!"
     elif temperature > 85:
         return "It's too dang hot out there!"
